7c30d1be165a767f274c28d899d5cff1dff93593
Abhishek Pratap Singh <iapsingh@outlook.com>
2024-09-05 23:54:38 +0530

    Create checkdatatype.java

---
+++ checkdatatype.java
@@ -0,0 +1,25 @@
+public class checkdatatype {
+    public static void main(String[] args) {
+
+        Scanner check = new Scanner(System.in);
+        age 
+
+                Object obj1 = "Hello, World!";
+                Object obj2 = 12.45;
+                Object obj3 = 45.67;
+        
+                // Checking the data type using instanceof
+                if (obj1 instanceof String) {
+                    System.out.println("obj1 is of type String");
+                }
+        
+                if (obj2 instanceof Double) {
+                    System.out.println("obj2 is of type Integer");
+                }
+        
+                if (obj3 instanceof Double) {
+                    System.out.println("obj3 is of type Double");
+                }
+            }
+
+}
